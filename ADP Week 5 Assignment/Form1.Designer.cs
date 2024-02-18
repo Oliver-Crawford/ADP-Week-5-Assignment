5d3d694a2e513c678fd67eaad8ddb0bbb1121e49
Oliver Crawford <oicrawford@gmail.com>
2024-02-17 22:57:07 -0600

    Add project files.

---
+++ ADP Week 5 Assignment/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ADP_Week_5_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtBEmpID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBEmpName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBEmpGender = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBEmpHireDate = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(13, 170);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(94, 170);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtBEmpID
+            // 
+            this.txtBEmpID.Location = new System.Drawing.Point(94, 200);
+            this.txtBEmpID.Name = "txtBEmpID";
+            this.txtBEmpID.Size = new System.Drawing.Size(200, 20);
+            this.txtBEmpID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EmpID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "EmpName";
+            // 
+            // txtBEmpName
+            // 
+            this.txtBEmpName.Location = new System.Drawing.Point(94, 226);
+            this.txtBEmpName.Name = "txtBEmpName";
+            this.txtBEmpName.Size = new System.Drawing.Size(200, 20);
+            this.txtBEmpName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "EmpGender";
+            // 
+            // txtBEmpGender
+            // 
+            this.txtBEmpGender.Location = new System.Drawing.Point(94, 252);
+            this.txtBEmpGender.Name = "txtBEmpGender";
+            this.txtBEmpGender.Size = new System.Drawing.Size(200, 20);
+            this.txtBEmpGender.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "EmpHiringDate";
+            // 
+            // txtBEmpHireDate
+            // 
+            this.txtBEmpHireDate.Location = new System.Drawing.Point(94, 278);
+            this.txtBEmpHireDate.Name = "txtBEmpHireDate";
+            this.txtBEmpHireDate.Size = new System.Drawing.Size(200, 20);
+            this.txtBEmpHireDate.TabIndex = 9;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(175, 169);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(84, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update By ID";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(265, 169);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(84, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete By ID";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBEmpHireDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBEmpGender);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBEmpName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBEmpID);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtBEmpID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBEmpName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBEmpGender;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBEmpHireDate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ ADP Week 5 Assignment/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace ADP_Week_5_Assignment
+{
+    public partial class Form1 : Form
+    {
+        static string dbName = "Employees.db";
+        static string tableName = "Employees";
+        SQLiteConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                conn = new SQLiteConnection($"Data Source={dbName}; Version = 3; New = True; Compress = True; ");
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand($"SELECT name FROM sqlite_master WHERE type='table';", conn);
+                SQLiteDataReader dr = cmd.ExecuteReader();
+                //checks and sees if rows exist, if they don't exists then it creates the table and populates it with the required data.
+                if (!dr.HasRows)
+                {
+                    //dr must be closed before command text can be changed
+                    dr.Close();
+                    cmd.CommandText = $"create table {tableName}(EmpID integer primary key, EmpName nvarchar(50), EmpGender nvarchar(1), EmpHiringDate date)";
+                    dr = cmd.ExecuteReader();
+                    dr.Close();
+                    cmd.CommandText = $"insert into {tableName} values(1, 'Employee 1', 'F', '2020-01-01'), (2, 'Employee 2', 'F', '2020-02-01'), (3, 'Employee 3', 'M', '2020-03-01'), (4, 'Employee 4', 'F', '2020-04-01')";
+                    dr = cmd.ExecuteReader();
+                    dr.Close();
+                }
+                //update the table
+                btnRead_Click(null, null);
+                conn.Close();
+                
+                
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SQLiteConnection($"Data Source={dbName}; Version = 3; New = True; Compress = True; ");
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand($"SELECT * from {tableName};", conn);
+                SQLiteDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SQLiteConnection($"Data Source={dbName}; Version = 3; New = True; Compress = True; ");
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand($"insert into {tableName} values({txtBEmpID.Text}, '{txtBEmpName.Text}', '{txtBEmpGender.Text}', '{txtBEmpHireDate.Text}');", conn);
+                SQLiteDataReader dr = cmd.ExecuteReader();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SQLiteConnection($"Data Source={dbName}; Version = 3; New = True; Compress = True; ");
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand($"update {tableName} set EmpName = '{txtBEmpName.Text}', EmpGender = '{txtBEmpGender.Text}', EmpHiringDate = '{txtBEmpHireDate.Text}' where EmpID = {txtBEmpID.Text};", conn);
+                SQLiteDataReader dr = cmd.ExecuteReader();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SQLiteConnection($"Data Source={dbName}; Version = 3; New = True; Compress = True; ");
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand($"delete from {tableName} where EmpID = {txtBEmpID.Text};", conn);
+                SQLiteDataReader dr = cmd.ExecuteReader();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
